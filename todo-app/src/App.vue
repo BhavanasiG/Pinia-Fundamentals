eaf5b05a5a9fd6b8f8f5e5e5e91e7b87c715ca0f
Ganesh Bhavanasi <ganesh.bhavanasi@fivium.co.uk>
2025-09-18 11:53:23 +0100

    L5 Deleting state from the store

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -6,12 +6,11 @@ import TodoApp from "./components/TodoApp.vue";
   <todo-app />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smooting: greyscale;
-  text-align: center;
-  color: #2c3e50;
+<style scoped>
+.todo-app {
+  padding: 5vh;
+  min-height: 100vh;
+  width: 30vw;
+  background-color: #f7f9fc;
 }
 </style>
